2849402aeca7a320893a3a252503bbbb825cee5e
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-03-18 16:22:54 +0530

    os introduction

---
+++ Introduction.java
@@ -0,0 +1,9 @@
+public class Introduction {
+    // ref: https://youtu.be/vBURTt97EkA?list=PLBlnK6fEyqRiVhbXDGLXDk_OQAeuVcp2O
+    // 1. An operating systems is a program that manages the computer hardware.
+    // 2. It also provides a basis for application programs and acts as an intermediary between computer user and computer hardware.
+
+    // users (system / application programs)-> operating system -> computer hardware
+    // 1. computer hardware - resources like CPU, Memory, I/O Devices (devices we use for giving input and getting output of the system)
+    // 2. application programs - wordprocessoor, spreadsheets, compilers, text editors, web browsers.
+}
