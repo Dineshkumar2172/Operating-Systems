e9d54bfbdd93b10a40c41cec4bb2f0b4760f9b93
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-03-18 17:02:29 +0530

    memory controller

--- OperatingSystemBasics.java
+++ OperatingSystemBasics.java
@@ -6,4 +6,11 @@ public class OperatingSystemBasics {
     //      We call the box next to our PC as CPU, but it's just a cabinet having motherboard, CD-ROM, Ports etc.
     //      But CPU is just the chip that's embeded in the motherboard
     //      Modern computers can have one or more of the CPU's depending on the power of our machine.
+
+    // 1. Each device controllers is in charge of a specific type of device. (disk controller for disk, USB controller for mouse keyboard and printer etc..., Video controller for monitor is etc)
+    // 2. The CPU and the device controllers can execute concurrently, competing for memory cycles. (all of these controllers are connected to a memory controller positioned before memory)
+    //          -> When something has to be loaded or executd, it has to be first loaded into main memory (or RAM) - main memory is finite (2GB of ram etc).
+    //          -> execute concurrently - means all the devices or controllers can execute all at a same time i.r. we can use keyboard while watching movie but we don't experience lag, because all of em work concurrently.
+    //          -> Like in above scenario, point 3 comes into play.
+    // 3. To ensure orderly access to the shared memory, a memory controller is provided whose function is to synchronise access to the memory.
 }
