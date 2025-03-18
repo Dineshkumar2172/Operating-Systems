963d0f6b226acff3f1695f4968310fbd64986d61
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-03-18 16:50:38 +0530

    cpu

--- OperatingSystemBasics.java
+++ OperatingSystemBasics.java
@@ -2,5 +2,7 @@ public class OperatingSystemBasics {
     // Some basic knowledge of the structure of computer system is required to understand how operating system work.
     // -> A modern general purpose computer consist of one or more CPUs and a number of device controllers connected through a common bus that provides access to shared memory.
     // What is a CPU? Brain of the computer.
-
+    //      We call the box next to our PC as CPU, but it's just a cabinet having motherboard, CD-ROM, Ports etc.
+    //      But CPU is just the chip that's embeded in the motherboard
+    //      Modern computers can have one or more of the CPU's depending on the power of our machine.
 }
