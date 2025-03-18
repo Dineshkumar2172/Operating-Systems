416c89cc26079b2b8058fc874d6f3d2561974d43
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-03-18 16:29:46 +0530

    OS types and goals

--- Introduction.java
+++ Introduction.java
@@ -3,7 +3,27 @@ public class Introduction {
     // 1. An operating systems is a program that manages the computer hardware.
     // 2. It also provides a basis for application programs and acts as an intermediary between computer user and computer hardware.
 
-    // users (system / application programs)-> operating system -> computer hardware
+    // users (system / application programs)-> operating system -> computer hardware (OS sits between user and hardware)
     // 1. computer hardware - resources like CPU, Memory, I/O Devices (devices we use for giving input and getting output of the system)
     // 2. application programs - wordprocessoor, spreadsheets, compilers, text editors, web browsers.
+
+    /*
+     * Types of operating systems:
+     *      1. Batch OS
+     *      2. Time sharing OS
+     *      3. Distributes OS
+     *      4. Network OS
+     *      5. Real Time OS
+     *      6. Multi programming / processing / tasking OS
+     * 
+     * Goals of OS: (how does the computer know to open a file once you double click on it? os is at play)
+     *      1. Convenience
+     *      2. Efficiency
+     *      3. Both
+     * 
+     * Functions of OS:
+     *      1. It is an interface between User & Hardware
+     *      2. Allocation of Resources
+     *      3. Management of Memory, Security etch.
+     */
 }
